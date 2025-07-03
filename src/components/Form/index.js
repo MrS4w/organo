c32067b5d816193d92f7152b7ee36c25cba1b16c
Victor <adsvictor@outlook.com>
2025-07-02 23:35:31 -0300

    ✨ feat: add Dropdown and Button components

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,14 +1,41 @@
+import Button from "../Button";
+import Dropdown from "../Dropdown";
 import TextField from "../TextField";
 import "./Form.css";
 
 const Form = () => {
+  const times = [
+    "Programação",
+    "Front-End",
+    "Data Science",
+    "Devops",
+    "UX e Design",
+    "Mobile",
+    "Inovação e Gestão",
+  ];
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Form submitted");
+  };
+
   return (
     <section className="form">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <TextField label="Nome" placeholder="Digite seu nome" />
-        <TextField label="Cargo" placeholder="Digite seu cargo" />
+        <TextField
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite seu nome"
+        />
+        <TextField
+          obrigatorio={true}
+          label="Cargo"
+          placeholder="Digite seu cargo"
+        />
         <TextField label="Imagem" placeholder="Informe o endereço do imagem" />
+        <Dropdown obrigatorio={true} itens={times} />
+        <Button>Criar Card</Button>
       </form>
     </section>
   );
